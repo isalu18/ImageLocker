239a18bdc87667fc79c5acfd0db9c7098984109b
isalu18 <luke_shakz@hotmail.com>
2021-04-30 16:47:28 -0700

    Project finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
+
+  //State para tomar input de Formulario
+  const [ busqueda, guardarBusqueda ] = useState('');
+  
+  const [ imagenes, guardarImagenes ] = useState([]);
+
+  //Paginacion
+  const [ paginaactual, guardarPaginaActual ] = useState(1);
+  const [ totalpaginas, guardarTotalPaginas ] = useState(1);
+
+  useEffect(() => {
+    
+    const consultarAPI = async () => {
+      if(busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = '21414296-eee0c6c229777aad2a4563e71';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      
+      guardarImagenes(resultado.hits);
+
+      //Calcular el total de paginas a crear
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(calcularTotalPaginas);
+
+      //Mover imagen hacia arriba
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth'})
+    }
+
+    consultarAPI();
+  }, [busqueda, paginaactual]);
+
+  //Definir pagina anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+
+    if(nuevaPaginaActual === 0) return;
+
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+
+  //Definir pagina siguiente
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaactual + 1;
+
+    if(nuevaPaginaActual > totalpaginas) return;
+
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+
   return (
     <div className="container">
+
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imagenes</p>
 
-        <Formulario/>
+        <Formulario
+          guardarBusqueda={ guardarBusqueda }
+        />
       </div>
+      <div className="row justify-content-center ">
+        <ListadoImagenes
+          imagenes={ imagenes }
+        />
+
+        { (paginaactual === 1)
+          ? null :
+          (<button
+            type="button"
+            className="bbtn btn-info mr-1"
+            onClick={ paginaAnterior }
+          >&laquo; Anterior</button>)
+        }
+
+        { (paginaactual === totalpaginas)
+          ? null :
+          (<button
+            type="button"
+            className="bbtn btn-info ml-1"
+            onClick={ paginaSiguiente }
+          >Siguiente &raquo;</button>)
+        }
+      </div>
+
     </div>
   );
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error';
 
-const Formilario = () => {
+const Formulario = ({ guardarBusqueda }) => {
+
+    //State del input
+    const [ palabra, guardarPalabra ] = useState('');
+
+    const [ error, guardarError ] = useState(false);
+
+    //Al dar submit
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        //Validar
+        if(palabra.trim() === '') {
+            guardarError(true);
+
+            return;
+        }
+
+        guardarError(false);
+
+        //Enviar al componente principal
+        guardarBusqueda(palabra);
+    }
+
     return ( 
-        <div className="row">
-            <div className="form-group col-md-8">
-                <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Busca una imagen. Ejemplo: gatos"
-                />
+        <form
+            onSubmit={ buscarImagenes }
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen. Ejemplo: gatos"
+                        onChange={ e => guardarPalabra(e.target.value) }
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-lg btn-danger btn-block"
+                        value="Buscar"
+                    />
+                </div>
             </div>
-            <div className="form-group col-md-4">
-                <input
-                    type="submit"
-                    className="btn btn-lg btn-danger btn-block"
-                    value="Buscar"
-                />
-            </div>
-        </div>
+
+            { error ? <Error mensaje="Agrega un termino de busqueda"/> : null }
+        </form>
      );
 }
  
-export default Formilario;
+export default Formulario;
