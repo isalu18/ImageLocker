ec99a933627a09f9e2bb5e3947e637667384a3f7
isalu18 <luke_shakz@hotmail.com>
2021-05-02 11:08:06 -0700

    Adding proptypes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Error from './Error';
+import PropTypes from 'prop-types';
 
 const Formulario = ({ guardarBusqueda }) => {
 
@@ -50,6 +51,10 @@ const Formulario = ({ guardarBusqueda }) => {
             { error ? <Error mensaje="Agrega un termino de busqueda"/> : null }
         </form>
      );
+}
+
+Formulario.propTypes = {
+    guardarBusqueda: PropTypes.func.isRequired
 }
  
 export default Formulario;
